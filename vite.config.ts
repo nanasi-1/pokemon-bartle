50f42ba0ec19e44fcfeb859d972d7eea14b20144
nanasi-1 <test-email>
2025-01-14 14:06:04 +0900

    サイトマップのパスを修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    sitemap()
+    sitemap({ 
+      hostname: 'https://nanasi-1.github.io/',
+      basePath: '/pokemon-bartle'
+    })
   ],
   base: './'
 })
