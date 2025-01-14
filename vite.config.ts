732d7fbdef15ab21c5420a4d2b174fc282ce528e
nanasi-1 <test-email>
2025-01-14 13:59:57 +0900

    サイトマップを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import sitemap from 'vite-plugin-sitemap'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap()
+  ],
   base: './'
 })
